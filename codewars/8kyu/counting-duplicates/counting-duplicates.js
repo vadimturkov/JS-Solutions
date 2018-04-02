8510beb5f6fb8f88dcd73a09154485725382a908
Vadim Turkov <vadim.turkov@c-map.com>
2018-04-02 13:04:31 +0300

    Add solutions header

--- codewars/8kyu/counting-duplicates/counting-duplicates.js
+++ codewars/8kyu/counting-duplicates/counting-duplicates.js
@@ -1,2 +1,5 @@
+// Solution to CodeWars kata: Counting Duplicates
+// https://www.codewars.com/kata/counting-duplicates/
+
 const duplicateCount = text => Array.from(text.toLowerCase()).filter((value, index, array) =>
     array.indexOf(value) !== index && array.lastIndexOf(value) === index).length;
